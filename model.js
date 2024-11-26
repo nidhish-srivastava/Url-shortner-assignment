72104f1fd8de5494ddeee5eb0950d8dc49cd1050
Nidhish Srivastava <nidhish0430@gmail.com>
2024-11-26 17:56:00 +0530

    created indexing and added deployment configs

--- model.js
+++ model.js
@@ -3,9 +3,12 @@ const Schema = mongoose.Schema;
 
 const urlSchema = new Schema({
     originalUrl: { type: String, required: true },
-    shortId: { type: String, required: true, unique: true },
+    shortId: { type: String, required: true, unique: true, index: true },  // Index added here
     clicks: { type: Number, default: 0 },
     lastAccessed: { type: Date, default: null }
 });
+
+// Create an index on the shortId field(which tells db to create indexing in ascending order)
+urlSchema.index({ shortId: 1 });
 
 module.exports = mongoose.model('URL', urlSchema);
